b07623003f38abfcd05455b4d48aac716ea30f5e
Mudit-Soral <muditsoral@gmail.com>
2026-02-20 17:46:56 +0530

    Add Whitenoise static files storage configuration

--- support_ticket/settings.py
+++ support_ticket/settings.py
@@ -142,3 +142,5 @@ LOGOUT_REDIRECT_URL = "/login/"
 import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
